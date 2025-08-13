530ab33daf683073dc88073ebfa06996d8b27696
João Pedro <joaopedrosantanarosa@gmail.com>
2025-08-13 10:27:40 -0300

    Ex-05
    Valores aritméticos.

---
+++ ex05.py
@@ -0,0 +1,7 @@
+n1 = int(input('Digite um valor: \n'))
+
+print('O dobro de {} é {}'.format(n1, (n1*2)))
+print('O triplo de {} é {}'.format(n1, (n1*3)))
+print('A raiz quadrada de {} é {}'.format(n1, pow(n1, (1/2))))
+
+#Utilizando formas aritméticas.
