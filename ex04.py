6f5f0bed6b7921010e9d5364f08e5d4bbe7d9540
João Pedro <joaopedrosantanarosa@gmail.com>
2025-08-13 10:13:41 -0300

    Ex-04
    Antecessor e sucessor somente com uma variável para evitar alocar muita memoria.

---
+++ ex04.py
@@ -0,0 +1,5 @@
+n1 = int (input('Digite um valor númerico inteiro: \n'))
+
+print('Analisando o valor {}, é possível afirmar que seu antecessor é {} e seu sucessor é {}, estou correto?'.format(n1, (n1-1), (n1+1)))
+
+#Simples equação de antecessor e sucessor sem armazenar a memoria do valor.
