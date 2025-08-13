148dc5e6d111ac658a99a9a0d9b2b2ff983b85e3
João Pedro <joaopedrosantanarosa@gmail.com>
2025-08-13 09:26:37 -0300

    Ex-02
    Utilizando .format.

--- ex02.py
+++ ex02.py
@@ -6,4 +6,8 @@ num2 = int (input('Digite outro valor inteiro para efetuar a soma: \n'))
 
 valorTotal = num1 + num2
 
-print('A soma dos valores é:', valorTotal)
+#print('A soma entre', num1, 'e', num2, 'é igual:', valorTotal)
+
+#Utilizando o .format para melhorar a qualidade do código.
+
+print('A soma entre {} e {} é igual: {}'.format(num1,num2, valorTotal))
