f4e5af39391e0488dc6ba0e7369bf1f041cde3cf
João Pedro <joaopedrosantanarosa@gmail.com>
2025-08-13 09:42:37 -0300

    Ex-03
    Mostrando o valor primitivo junto com o método IS para verificar se é ou não (bool).

---
+++ ex03.py
@@ -0,0 +1,9 @@
+n1 = input('Digite qualquer dígito: \n')
+
+#print(type(n1), n1.isnumeric()) - Verifica se é alfanumérico
+
+#print(type(n1), n1.isalpha()) - Verficar se o dígito faz parte do alfabeto.
+
+#print(type(n1), n1.isupper()) - Verifica se todos os dígitos são em letras maiúsculas.
+
+#print (type(n1), n1.islower()) - Verifica se todos os dígitos são em letras minúsculas.
