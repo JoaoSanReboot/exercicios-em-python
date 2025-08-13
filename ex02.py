e74f4468330405654d285980f9a486a4f8700f66
João Pedro <joaopedrosantanarosa@gmail.com>
2025-08-12 22:08:39 -0300

    Ex-02
    Relembrando valores primitivos.

---
+++ ex02.py
@@ -0,0 +1,9 @@
+#Fundamentos primitivos.
+
+num1 = int (input('Digite um valor inteiro para efetuar a soma: \n'))
+
+num2 = int (input('Digite outro valor inteiro para efetuar a soma: \n'))
+
+valorTotal = num1 + num2
+
+print('A soma dos valores é:', valorTotal)
