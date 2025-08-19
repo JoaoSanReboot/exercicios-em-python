02298a117956d47959f634802c834d4e0d6ab426
João Pedro <joaopedrosantanarosa@gmail.com>
2025-08-18 22:41:29 -0300

    Ex-08
    Tabuada!

---
+++ ex08.py
@@ -0,0 +1,17 @@
+#Escrever um programa que leia um valor inteiro e mostre a tabuada completa deste número.
+
+n1 = int(input('Digite um valor inteiro para exibir sua tabuada: \n'))
+
+print('-----------\n'
+      '6 x  1 = {} \n'
+      '6 x  2 = {} \n'
+      '6 x  3 = {} \n'
+      '6 x  4 = {} \n'
+      '6 x  5 = {} \n'
+      '6 x  6 = {} \n'
+      '6 x  7 = {} \n'
+      '6 x  8 = {} \n'
+      '6 x  9 = {} \n'
+      '6 x 10 = {} \n'
+      '-----------'.format((n1*1),(n1*2),(n1*3),(n1*4),(n1*5),(n1*6),(n1*7),(n1*8),(n1*9),(n1*10),))
+
