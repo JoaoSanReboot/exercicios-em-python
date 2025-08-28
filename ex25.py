ed593b2d0e12d7be5dc1bb8423adb24321e0477c
João Pedro <joaopedrosantanarosa@gmail.com>
2025-08-27 21:26:44 -0300

    Ex-25
    Utilizando e explorando mais o operador IN.

--- ex25.py
+++ ex25.py
@@ -1,1 +1,8 @@
 #Crie um programa que leia o nome de uma pessoa e diga se ela tem "Silva" no nome.
+
+nome = str(input('Qual seu nome completo? ')).split()
+
+nome = nome.title()
+
+print('Silva' in nome)
+
