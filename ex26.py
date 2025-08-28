49f0c5f33941591f60ea5e50f12acfb84d36ac23
João Pedro <joaopedrosantanarosa@gmail.com>
2025-08-27 21:51:56 -0300

    Ex-26
    Mais métodos de tranformação.

--- ex26.py
+++ ex26.py
@@ -2,3 +2,12 @@
 #Quantas vezes aparece a letra "A".
 #Em que posição ela aparece pela primera vez.
 #Em que posição ela aparece a última vez.
+
+frase = str(input('Digite uma frase aleatória: ')).strip()
+
+frase = frase.upper()
+
+print('Na sua frase a letra A aparece {} vezes.'. format(frase.count('A')))
+print('Na sua frase a letra A aparece pela primeira vez na posição {}.'. format(frase.find('A')+1))
+print('Na sua frase a letra A aparece pela útlima vez na posição {}.'. format(frase.rfind('A')+1))
+
