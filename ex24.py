f77a67cb021a3530abe2d153bc212102f969e32c
João Pedro <joaopedrosantanarosa@gmail.com>
2025-08-27 21:15:58 -0300

    Ex-24
    Utilizando métodos de transformação.

--- ex24.py
+++ ex24.py
@@ -1,1 +1,9 @@
-#Faça um programa que leia o nome de uuma cidade e diga se ela começa ou não com o nome "Santo".
+#Faça um programa que leia o nome de uma cidade e diga se ela começa ou não com o nome "Santo".
+
+cidade = str(input('Informe o nome de uma cidade: ')).strip()
+
+cidade = cidade.capitalize()
+
+print('Santo' in cidade)
+
+ 
