5bed819602055afab9661360a3fea6c518d934c2
João Pedro <joaopedrosantanarosa@gmail.com>
2025-08-27 22:08:15 -0300

    Ex-27
    Testando o Split.

--- ex27.py
+++ ex27.py
@@ -1,2 +1,8 @@
 #Faça um programa que leia o nome completo de uma pessoa, mostrando em seguida o primeiro e último nome separadamente.
 #Ex: Ana Maria de Souza - Primeiro: Ana, último: Souza
+
+nome = str(input('Digite seu nome:')).strip()
+
+nomeSplitado = nome.split()
+
+print('Seu primeiro nome é {} e seu último nome é {}, correto?'.format(nomeSplitado[0], nomeSplitado[-1]))
