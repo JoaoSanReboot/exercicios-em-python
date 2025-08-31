a0e43c05c870e46fc47627a1272e08b3601ac8b3
João Pedro <joaopedrosantanarosa@gmail.com>
2025-08-31 17:19:47 -0300

    Ex-30
    Clássico exercício de ímpar ou par.

--- ex30.py
+++ ex30.py
@@ -1,1 +1,10 @@
 #Crie um programa que leia um número inteiro e mostre na tela se ele é impar ou par.
+
+num = int(input('Informe um número aleatório que eu vou identificar se ele é Ímpar ou Par: \n'))
+
+resultado = num % 2
+
+if resultado == 0:
+    print('O número {} é Par.'.format(num))
+else:
+    print('O número {} é Ímpar.'.format(num))
