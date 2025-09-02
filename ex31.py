93cffabd4d8796e56fb4ed67151c508c785fe262
João Pedro <joaopedrosantanarosa@gmail.com>
2025-09-01 21:05:11 -0300

    Ex-31
    Praticando If e Else.

--- ex31.py
+++ ex31.py
@@ -1,2 +1,12 @@
 #Desenvolva um programa que pergunte a distância de uma viagem em Km. Calcule o preço da passagem, cobrando R$0,50 por km
 #para viagens até 200km e R$0,45 para viagens mais longas.
+
+quilometragem = int(input('Qual a distância da sua viagem planejada em quilometros? '))
+
+if quilometragem <= 200:
+    valor = quilometragem * 0.50
+else:
+    valor = quilometragem * 0.45
+
+print('Sua viagem de {:.1f}km está prestes a começar.'.format(quilometragem))
+print('O valor total da sua passagem será de R${:.2f}.'.format(valor))
