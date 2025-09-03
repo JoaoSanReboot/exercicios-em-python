e18ad9be5d73e2d5774b487c1775cecbb3d6e53d
João Pedro <joaopedrosantanarosa@gmail.com>
2025-09-02 21:04:58 -0300

    Ex-32
    Ano Bissexto!

--- ex32.py
+++ ex32.py
@@ -1,1 +1,15 @@
 #Faça um programa que leia um ano qualquer e mostre se ele é Bissexto.
+
+from datetime import date
+
+ano = int(input('Qual ano você quer vericar se foi ou será Bissexto ? \n'
+    'Caso queira analisar o ano atual digite 0: '))
+
+if ano == 0:
+    ano = date.today().year
+
+if ano % 4 == 0 and ano % 100 != 0 or ano % 400 == 0:
+    print('O ano {} é Bissexto!'.format(ano))
+
+else:
+    print('O ano {} não é Bissexto.'.format(ano))
