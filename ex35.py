ea50d3343638c7f6deee0c7ef71483639e7ac561
João Pedro <joaopedrosantanarosa@gmail.com>
2025-09-03 22:56:16 -0300

    Ex-35
    Verificando se 3 retas podem ou não formar um triângulo baseando-se no seu comprimento.

--- ex35.py
+++ ex35.py
@@ -1,1 +1,11 @@
 #Desenvolva um programa que leia o comprimento de três retas e diga ao usuário se elas podem ou não formar um triângulo.
+
+print('Informe três segmentos: \n')
+a = float(input('Primera reta: '))
+b = float(input('Segunda reta: '))
+c = float(input('Terceira reta: '))
+
+if a < b + c and b < a + c and c < a + b:
+    print('Os segmentos acimas podem sim formar um trângulo!!')
+else:
+    print('O segmentos indicados não podem formar um triângulo.')
