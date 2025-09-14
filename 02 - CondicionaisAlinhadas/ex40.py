01aafbd01d328521d048edf94a643f8bdc2db74d
João Pedro <joaopedrosantanarosa@gmail.com>
2025-09-14 19:02:32 -0300

    Ex-40
    Calculadora de médias.

--- 02 - CondicionaisAlinhadas/ex40.py
+++ 02 - CondicionaisAlinhadas/ex40.py
@@ -2,3 +2,21 @@
 # Média abaixo de 5.0 - Reprovado.
 # Média entre 5.0 e 6.9 - Recuperação.
 # Média 7.0 ou superior - Aprovado.
+
+print('-' * 32)
+print('Calculadora de média UniEnsinoJS')
+print('-' * 32)
+
+notaum = float(input('\nDigite a nota da sua primeira prova: '))
+notadois = float(input('\nDigite a nota da sua segunda prova: '))
+
+media = (notadois + notaum) / 2
+
+if media < 5.0:
+    print('Sua média final é de {} você está \033[31mReprovado\033[m!!'.format(media))
+
+elif media <= 6.9:
+    print('Sua média final é de {} você está de \033[31mRecuperação\033[m estude mais!!'.format(media))
+
+else:
+    print('Sua média final é de {} você está \033[32mAprovado\033[m. Parabéns!!!'.format(media))
