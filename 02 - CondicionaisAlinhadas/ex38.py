d4d53e8b4667ddd12ae6ec18fade5ffcf1640b55
João Pedro <joaopedrosantanarosa@gmail.com>
2025-09-14 18:20:10 -0300

    Ex-38
    Verificador de maior número.

--- 02 - CondicionaisAlinhadas/ex38.py
+++ 02 - CondicionaisAlinhadas/ex38.py
@@ -2,3 +2,15 @@
 #Primeiro valor é maior
 #Segundo valor é maior
 #Não existe valor maior, os dois são iguais.
+
+numum = int(input('Digite o valor do primeiro número: '))
+numdois = int(input('\nDigite o valor do segundo número: '))
+
+if numum > numdois:
+    print('O primeiro valor é o maior.')
+
+elif numdois > numum:
+    print('O segundo valor é o maior.')
+
+else:
+    print('Ambos são valores iguais não há um número maior.')
