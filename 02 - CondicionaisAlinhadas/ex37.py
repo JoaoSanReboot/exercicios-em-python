76f8ef6c254ea19f2306589e4a1aed7ebd55e63d
João Pedro <joaopedrosantanarosa@gmail.com>
2025-09-14 18:13:33 -0300

    Ex-37
    Conversor de bases com um número inteiro.

--- 02 - CondicionaisAlinhadas/ex37.py
+++ 02 - CondicionaisAlinhadas/ex37.py
@@ -2,3 +2,28 @@
 # 1 para Binário.
 # 2 para Octal.
 # 3 para Hexadecimal.
+
+baseum = int (input('Digite um número inteiro: '))
+print('\n Escolha uma das bases abaixo para efetuar a conversão: \n')
+print('[ 1 ] Converter para Binário. \n')
+print('[ 2 ] Converter para Octal. \n')
+print('[ 3 ] Converter para Hexadecimal. \n')
+opcao = int (input('Digite uma das opções acima: '))
+
+if opcao == 1:
+
+    basebinario = bin(baseum)[2:]
+    print('O número {} convertido para Binário é igual a {}.'.format(baseum, basebinario))
+
+elif opcao == 2:
+
+    baseoctal = oct(baseum)[2:]
+    print('O número {} convertido para Octal é igual a {}.'.format(baseum, baseoctal))
+
+elif opcao == 3:
+
+    basehexa = hex(baseum)[2:]
+    print('O número {} convertido para Octal é igual a {}.'.format(baseum, basehexa))
+
+else:
+    print('\n Você não digitou nenhuma das opções indicadas, por gentileza realize novamente a atividade.')
