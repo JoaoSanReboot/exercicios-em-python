9b1f77206480e0fc9c61380ab71fd73e49665846
João Pedro <joaopedrosantanarosa@gmail.com>
2025-09-15 20:44:15 -0300

    Ex-41
    Praticando mais Condicionais Alinhadas com categorias de natação.

--- 02 - CondicionaisAlinhadas/ex41.py
+++ 02 - CondicionaisAlinhadas/ex41.py
@@ -4,3 +4,24 @@
 # Até 19 anos - Junior.
 # Até 20 anos = Sênior.
 # Acima - Master.
+
+from datetime import date
+
+print('-' * 53)
+print('Seja Bem-Vindo(a) A Confederação Nacional de Natação!')
+print('-' * 53)
+ano = int(input('Informe seu ano de nascimento para verificarmos sua categoria: '))
+
+anoatual = date.today().year
+idade = anoatual - ano
+
+if idade <= 9:
+    print('\nSua categoria é \033[34mMirim.\033[m')
+elif idade <= 14:
+    print('\nSua categoria é \033[35mInfantil.\033[m')
+elif idade <= 19:
+    print('\nSua categoria é \033[36mJunior.\033[m')
+elif idade <= 25:
+    print('\nSua categoria é \033[31mSenior.\033[m')
+else:
+    print('\nSua categoria é \033[32mMaster.\033[m')
