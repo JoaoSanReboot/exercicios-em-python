ccdbc7d08024d261e507262c3f6299f7d3e75d92
João Pedro <joaopedrosantanarosa@gmail.com>
2025-09-22 22:29:43 -0300

    Ex-47
    Contador de números pares.

--- 03 - EstruturadeRepetição/ex47.py
+++ 03 - EstruturadeRepetição/ex47.py
@@ -1,1 +1,7 @@
 #Crie um programa que mostre na tela todos os números pares que estão no intervalo de 1 a 50.
+
+for c in range (1,51):
+    valor = c % 2
+    if valor == 0:
+        print(c)
+    
