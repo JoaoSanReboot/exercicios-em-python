6c0ca7aa9acafbc63c48cc47a6ab15bd4ced24c9
João Pedro <joaopedrosantanarosa@gmail.com>
2025-09-22 22:05:39 -0300

    Ex-46
    Contagem regressiva para explosão de fogos de artificio.

--- 03 - EstruturadeRepetição/ex46.py
+++ 03 - EstruturadeRepetição/ex46.py
@@ -1,2 +1,15 @@
 #Faça um programa que mostre na tela uma contagem regressiva para o estouro de fogos de artifício, indo de 10 até 0,
 #Com uma pause de um segundo entre eles.
+
+from time import sleep
+
+
+value = int(input('Vamos acender os fogos? Digite 1 para começara contagem:'))
+
+if value == 1:
+    for c in range(9, -2, -1):
+        print(c + 1)
+        sleep(1.5)
+    print('🎆🎆🎆')
+else:
+    print('Amigo. Dígito errado.')
