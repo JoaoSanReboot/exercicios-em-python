e6a486a591701c861fb70cdb4dcc7715544df8cf
João Pedro <joaopedrosantanarosa@gmail.com>
2025-09-24 21:44:55 -0300

    Ex-49
    Tabuada modificada com laço de repetição.

--- 03 - EstruturadeRepetição/ex49.py
+++ 03 - EstruturadeRepetição/ex49.py
@@ -1,1 +1,8 @@
 #Refaça o ex09, mostrando uma tabuada de um número que o usuário escolher, só que agora utilizando um laço for.
+
+tabuada = int(input('Digite o número que deseja saber a tabuada: '))
+
+for num in range(0, 11):
+    valorfinal = tabuada * num
+    print('{} x {} = {}'.format(tabuada,num,valorfinal))
+print('Fim da Tabuada!')
