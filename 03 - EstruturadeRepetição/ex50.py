5684309e7a2c945782c3545fb7d9fbba6b5fc6bd
João Pedro <joaopedrosantanarosa@gmail.com>
2025-09-24 21:53:39 -0300

    Ex-50
    Contador de números pares.

--- 03 - EstruturadeRepetição/ex50.py
+++ 03 - EstruturadeRepetição/ex50.py
@@ -1,1 +1,11 @@
-#Desenvolva um programa que leia seis números inteniros e mostre a soma apenas daqueles que forem pares. Se o valor for ímpar desconsidere-o.
+#Desenvolva um programa que leia seis números inteiros e mostre a soma apenas daqueles que forem pares. Se o valor for ímpar desconsidere-o.
+
+valorfinal = 0
+
+print('Digite seis números inteiros e farei a soma somente dos números pares.')
+
+for laco in range(1, 7):
+    numero = int(input('\nDigite o {}º número: '.format(laco)))
+    if numero % 2 == 0:
+        valorfinal += numero
+print('Somando somente os números pares indicados chegamos no valor {}, correto?'.format(valorfinal))
