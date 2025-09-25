a14f7bc6c3f61552fbe572e87297150a55d75e78
João Pedro <joaopedrosantanarosa@gmail.com>
2025-09-24 22:22:06 -0300

    Ex-51
    Calculadora de PA.

--- 03 - EstruturadeRepetição/ex51.py
+++ 03 - EstruturadeRepetição/ex51.py
@@ -1,1 +1,16 @@
 #Desenvolva um programa que leia o primeiro termo e a razão de uma PA. No final, mostre os 10 primeiros termos dessa progressão.
+
+print('=' * 30)
+print('     10 TERMOS DE UMA PA')
+print('=' * 30)
+
+valor = 0
+
+termo = int(input('\nPrimeiro termo:'))
+razao = int(input('\nRazão:'))
+print('\n')
+
+for num in range(1, 11):
+    valor = termo + (num - 1) * razao
+    print('{} '.format(valor), end='-> ')
+print('Acabou!')
