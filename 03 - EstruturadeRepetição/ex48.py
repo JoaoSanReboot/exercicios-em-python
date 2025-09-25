30a174dd62b819aee22dbdec87cec110d99c148c
João Pedro <joaopedrosantanarosa@gmail.com>
2025-09-24 21:37:00 -0300

    Ex-48
    Contador de números ímpares e múltiplos de 3, indo de 1 a 500.

--- 03 - EstruturadeRepetição/ex48.py
+++ 03 - EstruturadeRepetição/ex48.py
@@ -1,1 +1,10 @@
 #Faça um programa que calcule a soma entre todos os números ímpares que são múltiplos de três e que se encontram no intervalo de 1 até 500.
+
+value = 0
+contador = 0
+
+for c in range(1, 501):
+    if c % 3 == 0 and c % 2 != 0:
+           value += c
+           contador = contador + 1
+print('A soma dos {} números ímpares múltiplos de três é igual a {}'.format(contador,value))
