1318029f9c45882259e5b4d4bda09a3d1a96ddee
João Pedro <joaopedrosantanarosa@gmail.com>
2025-09-29 21:49:49 -0300

    Ex-52
    Verificador de números Primos.

--- 03 - EstruturadeRepetição/ex52.py
+++ 03 - EstruturadeRepetição/ex52.py
@@ -1,1 +1,19 @@
 #Faça um programa que leia um número inteiro e diga se ele é ou não um número primo.
+
+num = int(input('Digite um número e vou te dizer se ele é Primo ou não: \n'))
+
+tot = 0
+
+for c in range(1, num + 1):
+    if num % c == 0:
+        print(f'\033[33m{c}\033[m', end=' ')
+        tot += 1
+    else:
+        print(f'\033[31m{c}\033[m', end=' ')
+
+print('\n\nO número {} foi dívisível {} vezes'.format(num, tot))
+
+if tot == 2:
+    print('Por está razão ele \033[32mé\033[m um número Primo!')
+else:
+    print('Por está razão ele \033[31mnão\033[m é um número Primo')
